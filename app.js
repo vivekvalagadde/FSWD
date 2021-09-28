434dc1aa1183526bb9c361024bee5ff41f2d9e1d
vivekvalagadde <vivekvalagadde@gmail.com>
2021-09-28 19:38:32 +0530

    operators

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-console.log("Hello world")
-console.warn("Hello warning")
-console.error("Hello Error")
+//operators
+
+var a=6
+var b=3
+
+console.log('a+b=',a+b)
+console.log('b^a=',b**a)
+
+console.log('b=',b)
+console.log('b+=10',b+=10)
+
+var c=10
+
+var d='10'
+
+console.log(c==d)
+console.log(c===d)
+
+console.log(c + d)
+
+console.log(true&&true)
+console.log(true&&false)
+console.log(false&&true)
+console.log(false&&false)
+
+
+
