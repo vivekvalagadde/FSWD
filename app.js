77838cb67dd56aaa99723d84397fdf6563816bf0
vivekvalagadde <vivekvalagadde@gmail.com>
2021-09-29 19:54:46 +0530

    js elements

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-//operators
+var list = document.querySelector('ul')
+var arr = ['go to gym','cook meal', 'eat']
 
-var a=6
-var b=3
-
-console.log('a+b=',a+b)
-console.log('b^a=',b**a)
-
-console.log('b=',b)
-console.log('b+=10',b+=10)
-
-var c=10
-
-var d='10'
-
-console.log(c==d)
-console.log(c===d)
-
-console.log(c + d)
-
-console.log(true&&true)
-console.log(true&&false)
-console.log(false&&true)
-console.log(false&&false)
-
-
-
+arr.push('complete the assignment')
+for (var i=0;i<arr.length;i++) {
+    var element = document.createElement('li')
+    var textNode = document.createTextNode(arr[i])
+    element.appendChild(textNode)
+    list.appendChild(element)
+    
+}
