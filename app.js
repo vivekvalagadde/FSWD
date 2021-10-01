098c9c42b14dc14e84bc839403bd568964ebd5e6
vivekvalagadde <vivekvalagadde@gmail.com>
2021-10-01 19:06:07 +0530

    addition multiplication

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-var list = document.querySelector('ul')
-var arr = ['go to gym','cook meal', 'eat']
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
+const add = document.querySelector('#add')
+const multiply = document.querySelector('#multiply')
+const resultBox = document.querySelector('.result')
 
-arr.push('complete the assignment')
-for (var i=0;i<arr.length;i++) {
-    var element = document.createElement('li')
-    var textNode = document.createTextNode(arr[i])
-    element.appendChild(textNode)
-    list.appendChild(element)
-    
+const sum = () => {
+    const result=parseInt(a.value)+ parseInt(b.value)
+    resultBox.innerHTML = result
 }
+
+const product = () => {
+    const result=parseInt(a.value) * parseInt(b.value)
+    resultBox.innerHTML = result
+}
+
+const calculate = (event,operation) => {
+    console.log(event.target)
+    if(operation == 'add') {
+        console.log('adding')
+        return sum()
+    }
+    else if(operation == 'multiply') {
+        console.log('multiply')
+       return product()
+    }
+
+}
+
+add.addEventListener('click', (event) => {
+    return calculate(event, 'add')
+})
+multiply.addEventListener('click', (event) => {
+    return calculate(event, 'multiply')
+})
+
+
