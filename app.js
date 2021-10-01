4e2428af5cd2fb4f16f23a7addf681dc05f18d09
vivekvalagadde <vivekvalagadde@gmail.com>
2021-10-01 19:40:09 +0530

    calculator

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
 const a = document.querySelectorAll('input')[0]
 const b = document.querySelectorAll('input')[1]
 const add = document.querySelector('#add')
+const substract = document.querySelector('#substract')
 const multiply = document.querySelector('#multiply')
 const resultBox = document.querySelector('.result')
 
-const sum = () => {
-    const result=parseInt(a.value)+ parseInt(b.value)
-    resultBox.innerHTML = result
-}
+// const sum = () => {
+    // const result=parseInt(a.value)+ parseInt(b.value)
+    // resultBox.innerHTML = result
+// }
 
-const product = () => {
-    const result=parseInt(a.value) * parseInt(b.value)
-    resultBox.innerHTML = result
-}
+// const product = () => {
+    // const result=parseInt(a.value) * parseInt(b.value)
+    // resultBox.innerHTML = result
+// }
+
+// const substract = () => {
+    // const result=parseInt(a.value) - parseInt(b.value)
+    // resultBox.innerHTML = result
+// }
+
 
 const calculate = (event,operation) => {
-    console.log(event.target)
-    if(operation == 'add') {
-        console.log('adding')
-        return sum()
-    }
-    else if(operation == 'multiply') {
-        console.log('multiply')
-       return product()
+    // console.log(event.target)
+    // if(operation == 'add') {
+        // console.log('adding')
+        // return sum()
+    // }
+    // else if(operation == 'multiply') {
+        // console.log('multiply')
+    //    return product()
+    // }
+    switch (operation) {
+        case "add":
+            resultBox.innerHTML = parseInt(a.value) + parseInt(b.value);
+            break;
+        case "multiply":
+            resultBox.innerHTML = parseInt(a.value) * parseInt(b.value);
+            break;
+        case "substract":
+            resultBox.innerHTML = parseInt(b.value) - parseInt(a.value);
+            break;
+
+        
     }
 
 }
@@ -33,5 +53,7 @@ add.addEventListener('click', (event) => {
 multiply.addEventListener('click', (event) => {
     return calculate(event, 'multiply')
 })
-
+add.addEventListener('click', (event) => {
+    return calculate(event, 'substract')
+})
 
